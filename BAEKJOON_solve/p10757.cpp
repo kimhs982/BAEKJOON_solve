16879071f752e4c941e378ce57a2a0b6cf7d6670
kimhs982 <kimhyungsuk982@gmail.com>
2022-03-31 02:05:53 +0900

    p10757(3월 31일)-수정

--- BAEKJOON_solve/p10757.cpp
+++ BAEKJOON_solve/p10757.cpp
@@ -1,73 +1,85 @@
 // 10757번, 큰 수 A+B
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
+vector<short> big_plus1(const string&, const string&);
+vector<short> big_plus2(string&, string&);
+
 int main() {
-	int digit_A = 0, digit_B = 0, digit_result = 0;
-	char* A = new char[10002];
-	char* B = new char[10002];
-	char* result = new char[10002];
+	string A, B;
+	vector<short> result;
+	cin >> A >> B;
 
-	for (int i = 0; i < 10002; i++)
-		result[i] = 48;
+	result = big_plus2(A, B);
 
-	for (int i = 0; i < 10002; i++) {
-		A[i] = cin.get();
-		if (A[i] == ' ')
-			break;
-		digit_A = i;
-	}
-
-	for (int i = 0; i < 10002; i++) {
-		B[i] = cin.get();
-		if (B[i] == '\n')
-			break;
-		digit_B = i;
-	}
-
-	while ((digit_A >= 0) && (digit_B >= 0)) {
-		if (A[digit_A] + B[digit_B] + result[digit_result] >= 154) {
-			result[digit_result] += (A[digit_A] + B[digit_B] - 106);
-			result[digit_result + 1]++;
-		}
-		else {
-			result[digit_result] += (A[digit_A] + B[digit_B] - 96);
-		}
-		digit_A--;
-		digit_B--;
-		digit_result++;
-	}
-
-	while (digit_A >= 0) {
-		if ((result[digit_result] + A[digit_A]) == 106) {
-			result[digit_result] = 0;
-			result[digit_result + 1]++;
-		}
-		else
-			result[digit_result] += (A[digit_A] - 48);
-		digit_A--;
-		digit_result++;
-	}
-	while (digit_B >= 0) {
-		if ((result[digit_result] + B[digit_B]) == 106) {
-			result[digit_result] = 0;
-			result[digit_result + 1]++;
-		}
-		else
-			result[digit_result] += (B[digit_B] - 48);
-		digit_B--;
-		digit_result++;
-	}
-
-	if (result[digit_result] == 48)
-		digit_result--;
-
-	for (int i = digit_result; i >= 0; i--)
-		cout << short(result[i]) - 48;
-
-	delete[] A;
-	delete[] B;
-	delete[] result;
+	for (unsigned int i = 0; i < result.size(); i++)
+		cout << result[i];
 
 	return 0;
+}
+
+vector<short> big_plus1(const string& in_A, const string& in_B) {
+	vector<short> sum_result;
+	int index_A = in_A.length() - 1, index_B = in_B.length() - 1;
+	int index_com = (index_A > index_B ? index_A : index_B);
+
+	for (int i = index_A; i >= 0; i--)
+		sum_result.push_back(in_A[i] - '0');
+
+	if (index_A > index_B)
+		for (int i = 0; i <= index_B; i++)
+			sum_result[i] += (in_B[index_B - i] - '0');
+	else
+		for (int i = 0; i <= index_B; i++) {
+			if (i <= index_A)
+				sum_result[i] += (in_B[index_B - i] - '0');
+			else
+				sum_result.push_back(in_B[index_B - i] - '0');
+		}
+
+	for (int i = 0; i <= index_com; i++)
+		if (sum_result[i] >= 10) {
+			sum_result[i] %= 10;
+			if (i == index_com)
+				sum_result.push_back(1);
+			else
+				sum_result[i + 1]++;
+		}
+	reverse(sum_result.begin(), sum_result.end());
+
+	return sum_result;
+}
+
+vector<short> big_plus2(string& in_A, string& in_B) {
+	// in_A와 in_B string중에 길이가 더 긴 것이 in_A가 되도록 swap함
+	if (in_A.length() < in_B.length())
+		swap(in_A, in_B);
+
+	vector<short> sum_result;
+
+	// string in_A를 정수로 변환하여 sum_result에 저장
+	for (int i = in_A.length() - 1; i >= 0; i--)
+		sum_result.push_back(in_A[i] - '0');
+
+	// string in_B를 정수로 변환하여 sum_result에 더함
+	for (int i = 0; i < in_B.length(); i++)
+		sum_result[i] += (in_B[in_B.length() - i - 1] - '0');
+
+	// 10보다 크거나 같은 sum_result의 원소를 나머지 연산(%)을 통해서 10보다 작은 값으로 바꾸고, carry를 다음 원소에 넘김
+	for (int i = 0; i < sum_result.size(); i++)
+		if (sum_result[i] >= 10) {
+			sum_result[i] %= 10;
+			if (i == sum_result.size() - 1)
+				sum_result.push_back(1);
+			else
+				sum_result[i + 1]++;
+		}
+	// sum_result(vector)의 순서를 변경
+	reverse(sum_result.begin(), sum_result.end());
+
+	return sum_result;
 }
