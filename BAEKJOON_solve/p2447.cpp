21dc99315fd7f6bd5e6bd4deb33039b91da5b18c
kimhs982 <kimhyungsuk982@gmail.com>
2022-04-16 23:52:32 +0900

    2447번(4월 16일)-초안

---
+++ BAEKJOON_solve/p2447.cpp
@@ -0,0 +1,19 @@
+// 2447¹ø, º° Âï±â -10
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+
+void printStar_recursive(int);
+
+int main() {
+	int N;
+	scanf("%d", &N);
+
+	printStar_recursive(N);
+
+	return 0;
+}
+
+void printStar_recursive(int in_N) {
+
+}
